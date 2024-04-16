9d19f67b31662aaf974b8b98f0021b76a79c37ce
Arthur Aguiar <arthuraguiardevbr@gmail.com>
2024-04-16 13:31:09 -0400

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: "JavaScript Tutorial",
+    productNumber: "85743",
+    paymentStatus: "Atrasado",
+    status: "Aguardando",
+  },
+  {
+    productName: "CSS Full Course",
+    productNumber: "97245",
+    paymentStatus: "Reembolso",
+    status: "Recusado",
+  },
+  {
+    productName: "Flex-Box Tutorial",
+    productNumber: "36452",
+    paymentStatus: "Pago",
+    status: "Ativo",
+  },
+];
